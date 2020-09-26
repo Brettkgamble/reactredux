3be547f1b6c0e570ca331f43fbf91a662b3fc2a0
brettkgamble@gmail.com <5413CrossFit2018>
2020-09-26 12:08:24 -0600

    0002 - Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+import { connect }from 'react-redux';
+import { moviesList } from "./actions";
+import { bindActionCreators} from "redux";
 
-export default class App extends Component {
-  render() {
+class App extends Component {
+
+  componentDidMount() {
+      this.props.moviesList();
+  }
+
+    render() {
     return (
         <div>
-          Hello
+            {
+                this.props.movies ?
+                    this.props.movies.map((item) => (
+                        <div key ={item.id}>
+                            {item.name}
+                        </div>
+                    ))
+                : null
+            }
+
         </div>
     )
   }
 }
+
+function mapStateToProps(state) {
+    console.log(state)
+    return {
+        movies: state.movies
+    }
+}
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({ moviesList }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import reducers from './reducers';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware} from "redux";
+import promiseMiddleware from 'redux-promise';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore)
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(createStore)
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware()}>
+    <Provider store={createStoreWithMiddleware(reducers)}>
         <App />
     </Provider>
    , document.getElementById('root')
